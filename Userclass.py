d90999c10bb091767917dfd7f78603865298cbe1
mbaire <mbairemunge@gmail.com>
2021-09-30 02:47:26 +0300

    Created user class

---
+++ Userclass.py
@@ -0,0 +1,14 @@
+class User:
+
+# Add+Save new user
+
+Users = []
+def __init__(self, username, password):
+    self.username = username
+    self.password = password
+    
+def saveuser(self):
+    User.user_List. append(self)
+    
+def displayUsers(cls):
+        return cls.users
