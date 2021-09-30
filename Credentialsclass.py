a2450a1e8156ce4f2f279a75a4a53552f78c8e5c
mbaire <mbairemunge@gmail.com>
2021-09-30 03:18:40 +0300

    Created Credential class

---
+++ Credentialsclass.py
@@ -0,0 +1,15 @@
+class Credentials:
+    # Add+Save new credentials
+    
+    Credential_list = []
+    
+def __init__(self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password = password
+
+def saveCred(self):
+        Credentials.credential_list.append(self)
+
+def displayCredentials(cls):
+        return cls.credential_list
--- Userclass.py
+++ Userclass.py
@@ -2,13 +2,13 @@ class User:
 
 # Add+Save new user
 
-Users = []
+    Users = []
 def __init__(self, username, password):
     self.username = username
     self.password = password
     
 def saveuser(self):
     User.user_List. append(self)
-    
+
 def displayUsers(cls):
         return cls.users
