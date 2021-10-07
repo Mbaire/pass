085cfc969d1bd91d18907f132a587bc5171d6c1d
mbaire <mbairemunge@gmail.com>
2021-10-07 14:03:42 +0300

    Modified Users test file

--- Usertest.py
+++ Usertest.py
@@ -2,15 +2,15 @@ from Userclass import User
 import unittest
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.myUser= User("Mbairenat","123456789")
+        self.myUser= User("Mbairenat","12345678")
     def test__init__(self):
         self.assertEqual(self.myUser.username,"Mbairenat")
-        self.assertEqual(self.myUser.password,"123456789")
+        self.assertEqual(self.myUser.password,"12345678")
     def tearDown(self):
         User.users = []
     def test_save_user(self):
         self.myUser.saveUser()
-        test_user = User("Mbairenat","07123456789")
+        test_user = User("Mbairenat","0712345678")
         test_user.saveUser()
         self.assertEqual(len(User.users),2)
     def test_display_users(self):
---
+++ app.py
@@ -0,0 +1,72 @@
+from Userclass import User
+from Credentialsclass import Credentials
+from random import randint
+'''for the user class'''
+def createUser(username, password):
+    myUser = User(username, password)
+    return myUser
+def saveUser(user):
+    user.saveUser()
+def displayUsers():
+    User.displayUsers()
+    '''this is for the credentials class'''
+def createCredentials(account, username, password):
+    myCred = Credentials(account, username, password)
+    return myCred
+
+def saveCredentials(credentials):
+    credentials.saveCred()
+def displayCredentials():
+    return Credentials.displayCredentials()
+    
+def main():
+    print("Welcome to the Pass volt,")
+    print("\n")
+    while True: 
+        shortcodes= input("Do the following,type; nu -to create user account , du -to display user , ex -to exit:").lower()
+        if shortcodes == "nu":
+            username = input("Enter your username:")
+            password = input("Enter your password:")
+            saveUser(createUser(username, password))
+            print(f"Hello {username} your pass volt account has successfully been created")
+            print("\n")
+
+            while True:
+                print(f"Welcome {username} to the section of adding accounts and their passwords:")
+                print("-"*20)
+                code =input("To proceed, choose the following to interact with the application:  nc -to create credentials: dc -to display credentials:").lower()
+                if code == "nc":
+                    feedback = input("Enter y to create your own password and n for the system to generate:").lower()
+                    if feedback == ("y"):
+                        account = input("Enter the name of account, eg facebook, instagram, twitter:")
+                        user_name = input("Enter your desired username:")
+                        pass_word = input("Enter the password:")
+                        saveCredentials(createCredentials(account,username, password))
+                        print("Credentials successfully uploaded")
+                    elif feedback == "n":
+                        account = input("Enter the name of account, eg facebook, instagram, twitter:")
+                        user_name = input("Enter your desired username:")
+                        pass_word = randint(123456789,987456321)
+                        saveCredentials(createCredentials(account, username, password))
+                        print(f"Hello {user_name} Your password is {pass_word}......SAVED!!!")    
+                    else:
+                        print("We didnt what you really want!!!")
+                elif code == "dc":
+                    if displayCredentials():
+
+                        for credential in displayCredentials():
+                            print(f"Account: {credential.account} Username:{credential.user_name} Password:{credential.pass_word}")
+                    else:
+                        print("No accounts to display")
+                else:   print("Sorry,could not process what you want")
+                # if code == "ex":
+                        # print("Exit successful")
+         # below for the parent
+        elif shortcodes == "du":
+            for user in User.users:
+                print(f"{user.username}")
+        elif shortcodes == "ex":
+            print("Exit successful")
+            break
+        else: print("Kindly check your entry again")   
+main()
